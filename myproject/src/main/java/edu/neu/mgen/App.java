5ae9265e018d1fff8490bb397703979da57a6f53
terryye <i@terryye.com>
2024-10-05 07:54:50 -0700

    init project

---
+++ myproject/src/main/java/edu/neu/mgen/App.java
@@ -0,0 +1,15 @@
+package edu.neu.mgen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        // print text in the console
+        System.out.println("It is my first Java program!");
+    }
+}
+/**
+ * this is my multi-line comment.
+ */
